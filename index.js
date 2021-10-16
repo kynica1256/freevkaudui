6e8a4b3881d96a04c1c57e80180247934b3453b5
Kirill <69206145+kynica1256@users.noreply.github.com>
2021-10-16 15:37:22 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ const request = require('request');
 request(
     "https://moa.hotmo.org/search?q=Humans",
     (err, response, body) => {
-                  console.log(body)
+     let arr = body.split('\n');
+     for (let i of arr) {
+        console.log(i)
+        const text = i.replace(/^\s+|\s+(?=[\n\r])/gm,"");
+        const test = text.split('').slice(0, 23).join("");
+        if (test === '<a data-nopjax="" href=') {
+            const str = text.split('').slice(24, -34).join("");
+            console.log(str)
+        }
+     }
      }
 )
