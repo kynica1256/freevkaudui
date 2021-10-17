181dd1af457a91557a26bbb81ce7ee185fea5a6d
Kirill <69206145+kynica1256@users.noreply.github.com>
2021-10-17 11:20:20 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,36 +3,51 @@ const app = express()
 const request = require('request');  
 const { base64encode, base64decode } = require('nodejs-base64');
 
+app.use(express.static(__dirname + '/use'));
+
 
 var ress = true;
 
-var url = "";
 
+const url = {"url":""};
 app.get('/', function (req, res) {
+    console.log(base64decode(req.query.str))
     request(
         "https://moa.hotmo.org/search?q="+base64decode(req.query.str),
         (err, response, body) => {
-            let arr = body.split('\n');
-            for (let i of arr) {
-               const text = i.replace(/^\s+|\s+(?=[\n\r])/gm,"");
-               const test = text.split('').slice(0, 23).join("");
-               //console.log(test)
-               if (test == '<a data-nopjax href="ht') {
-                   const str = text.split('').slice(21, -34).join("");
-                   ress = false;
-                   url = str;
-               }
-               if (ress == false) {
-                   break;
-               }
-            }
-         }
+            try {
+                let arr = body.split('\n');
+                for (let i of arr) {
+                   var text = i.replace(/^\s+|\s+(?=[\n\r])/gm,"");
+                   var test = text.split('').slice(0, 23).join("");
+                   if (test == '<a data-nopjax href="ht') {
+                       var str = text.split('').slice(21, -34).join("");
+                       ress = false;
+                       url["url"] = str;
+                   }
+                   if (ress == false) {
+                       break;
+                   }
+                }
+                ress = true;
+            } catch (err) {}
+
+        }
     )
-    res.append('Access-Control-Allow-Origin', '*');
-    res.append('Access-Control-Allow-Credentials', 'true');
-    res.send("true");
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.send("{");
 })
 app.get('/check', function (req, res) {
-    res.send(url);
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    console.log(url["url"]);
+    res.send(url["url"]);
 })
-app.listen(3000)
+app.get('/music', function (req, res) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.sendfile('use/index.html');
+})
+app.listen(5000)
+
