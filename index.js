e59bb6459f6e5df7f1ce45550ae8194fcd1e1de2
Kirill <69206145+kynica1256@users.noreply.github.com>
2021-10-24 19:57:13 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-
 const express = require('express')
 const app = express()
 const request = require('request');  
 const { base64encode, base64decode } = require('nodejs-base64');
-
-
-
+var crypto = require("crypto");
+let fs = require("fs");
 var ress = true;
 
-const url = {"url":""};
+const url = {"url":"1"};
+
+const file = {"file":"","url":""};
+
 app.get('/', function (req, res) {
     console.log(base64decode(req.query.str))
     request(
@@ -29,6 +30,7 @@ app.get('/', function (req, res) {
                    }
                 }
                 ress = true;
+                request("http://127.0.0.1:5000/check");
             } catch (err) {}
 
         }
@@ -39,9 +41,20 @@ app.get('/', function (req, res) {
 })
 app.get('/check', function (req, res) {
     console.log(url["url"]);
-    res.send(url["url"]);
+    if (file["url"] != url["url"]) {
+        var id = crypto.randomBytes(10).toString('hex');
+        file["file"] = id+".mp3";
+        file["url"] = url["url"];
+        request(url["url"]).pipe(fs.createWriteStream(file["file"]));
+    }
+    res.send("{")
 })
-app.get('/music', function (req, res) {
-    res.sendfile('index.html');
+app.get('/musiccheck', function (req, res) {
+    console.log(file["file"])
+    res.sendfile(file["file"], {acceptRanges: false});
 })
 app.listen(5000)
+
+function base64_encode(file) {
+    return "data:image/gif;base64," + fs.readFileSync(file, 'base64');
+}
